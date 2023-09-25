863bbc16b20a5a2a6b56cc78357c1065be3ce53f
Harsh <harshmungra255@gmail.com>
2023-09-25 22:23:06 +0530

    taskmate site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { ShowTask } from './components/ShowTask';
 import './App.css';
 
+
 function App() {
+  const [taskList , setTasklist] =useState( JSON.parse( localStorage.getItem("taskList")) || []);
+  const [task , setTask] =useState({});
+
+ useEffect ( () => {
+    localStorage.setItem("taskList" , JSON.stringify(taskList))
+   }, [taskList] )
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask
+        taskList={taskList}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        taskList={taskList}
+        setTasklist={setTasklist}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
